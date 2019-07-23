e1db5a9c6a0f2cb4332d64f0b407c7e841039e80
shooter <shooter.hero@gmail.com>
2019-07-23 12:18:22 +0800

    fix mainnet address

--- src/pages/ckb.js
+++ src/pages/ckb.js
@@ -22,7 +22,7 @@ class BlogIndex extends React.Component {
 
   genAddress() {
     const privateKey = ec.genKeyPair()
-    const mainaddr = new Address(privateKey, { prefix: 'ckm' })
+    const mainaddr = new Address(privateKey, { prefix: 'ckb' })
     const testaddr = new Address(privateKey, { prefix: 'ckt' }) // the ckt is the signal for testnet
 
     this.setState({ privateKey: '0x'+testaddr.getPrivateKey(), mainnetaddress: mainaddr.value, testnetaddress: testaddr.value })
